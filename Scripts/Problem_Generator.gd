9d346cebb7a42e9e68abb455d817c55d9783b7ea
GRLT <laciok3@gmail.com>
2024-04-12 14:37:04 +0200

    Fixed spam emitting

--- Scripts/Problem_Generator.gd
+++ Scripts/Problem_Generator.gd
@@ -4,31 +4,47 @@ extends Node
 #between instructions. This should be the game.
 #
 
+#@onready var radio_view = get_node("MainGame/ChiperRadioView")
+
 const TIME_BETWEEN_PROBLEMS = 2
 
 signal chiper(problems)
+
 
 var problem_timer := Timer.new()
 
 var problems = []
 
+
 func _ready():
 	add_child(problem_timer)
 	problem_timer.one_shot = false
 	problem_timer.start(TIME_BETWEEN_PROBLEMS)
-	problem_timer.timeout.connect(on_problem_timer_timeout, 4)
+	problem_timer.timeout.connect(on_problem_timer_timeout)
+
+	#Connections from Views
+	#radio_view.stat.connect(radio_callback)
 	
 	#This probably needs some kinds of threading, because I want to
 	#be able to generate multiple problems that cloud queue up.
-	chiper_send_signal_on_timeout("ez")
-	chiper_send_signal_on_timeout("itt van a vilag")
-	chiper_send_signal_on_timeout("ua")
+	chiper_message("az")
+	chiper_message("af")
+	#chiper_message("av")
 
-func chiper_send_signal_on_timeout(message: String):
-	var chiper_random = randi_range(-24, 24)
-	var chiper_obj = Caesar_Shift_Object.new(message, chiper_random)
-	problems.push_front(chiper_obj)
+func radio_callback():
+	print("Arrived from Radio_View")
+
+func chiper_message(message: String):
+	#var chiper_random = randi_range(-24, 24)
+	var chiper_obj = Caesar_Shift_Object.new(message, 20)
+	problems.push_back(chiper_obj)
+
 	
 
 func on_problem_timer_timeout():
-	emit_signal("chiper", problems)
+	connect("stats", radio_callback)
+	if problems.size() > 0:
+		print("Sending Signal chiper")
+		emit_signal("chiper", problems[0])
+		problems.pop_front()
+	
