b3df89615d3f396446f54e273ce438bb31cd9abc
GRLT <laciok3@gmail.com>
2024-05-02 15:59:52 +0200

    Add the map minigame

--- Scripts/Classes/Map_Class.gd
+++ Scripts/Classes/Map_Class.gd
@@ -14,21 +14,37 @@ func _init(_width, _height, fill = null):
 			array[i].append(fill)
 			
 func getValue(x,y):
+	range_check(x,y)
 	return array[x][y]
 
+
 func setValue(x, y, value):
+	range_check(x,y)
+
+	assert(!(getValue(x,y) != 0), "The current coordinate is " + str(array[x][y]) + "already populated")
+	
 	array[x][y] = value
 
-
+func range_check(x,y):
+	assert(x >= 0 && x <= width-1, "X is out of range on Map")
+	assert(y >= 0 && y <= height-1, "Y is out of range in Map")
 	
-func setValueAroundPoint(map:Map, x, y, prettyPrint=false):
-	if x < 0 || x >= width:
-		assert(false, "Map width is larger then it's max width")
-	if y < 0 || y >= height:
-		assert(false, "Map HEIGHT is larger then it's max height")
+func find_points(value):
+	var point_array = []
+	for i in array.size():
+		for j in array.size():
+			if array[i][j] == value:
+				point_array.append([i,j])
+	return point_array
+	
+	
+func get_all_friendly_units():
+	pass
+			
+	
+func setValueAroundPoint(map:Map, x, y, value):
+	range_check(x,y)
 		
-
-
 	var top_left = {'x': x-1, 'y': y-1}
 	var top =  {'x': x-1, 'y': y}
 	var top_right = {'x': x-1, 'y': y+1}
@@ -40,9 +56,8 @@ func setValueAroundPoint(map:Map, x, y, prettyPrint=false):
 	
 	var around = [top_left, top, top_right, left, right, bottom_left, bottom, bottom_right]
 	
+	#Also checks if it's withing the array's range
 	for i in around:
 		if i.get('x') >= 0  && i.get('y') >= 0:
 			if i.get('x') <= width-1 && i.get('y') <= height-1:
-				map.setValue(i.get('x'),i.get('y'), 4)
-				if prettyPrint:
-					print("Point in X: ", i.get('x'), " ", "Point in Y: ", i.get('y'), " ", "Value is: ", map.getValue(i.get('x'),i.get('y')))
+				map.setValue(i.get('x'),i.get('y'), value)
--- Scripts/Map.gd
+++ Scripts/Map.gd
@@ -1,27 +1,78 @@
 extends Node
 
-#5 - Base
-#1 - Default Tile
-#2 - Forest Tile
-#3 - Enemy Tile
-#4 - Dead Tile
-#5 - Water Tile
 
 
+signal map_back
 
+var base_texture := preload("res://Assets/Sprites/base.png")
+var default_texture := preload("res://Assets/Sprites/rock.png")
+var friendly_texture := preload("res://Assets/Sprites/friendly_unit.png")
+var supply_texture := preload("res://Assets/Sprites/supply.png")
+
+@onready var back_button = $Back
+@onready var map_container = $GridContainer
+
+const MAP_WIDTH = 10
+const MAP_HEIGHT = 10
+
+func _process(delta):
+	refresh_tiles()
+	pass
+
+var mp := Map.new(MAP_WIDTH,MAP_HEIGHT,0)
 func _ready():
-	var mp = Map.new(10,10,1)
+	map_container.columns = MAP_WIDTH
+	
+	back_button.pressed.connect(scene_back_pressed)
+	ProblemGenerator.supply_drop_signal.connect(supply_drop)
 	
 	var starting_position_x: int = randi() % (mp.width - 1)
 	var starting_position_y: int = randi() % (mp.height - 1)
 	if(starting_position_x < 0 || starting_position_y < 0):
 		assert(false, "Random Value in Map less then 0")
 	
+
+	refresh_tiles()
+	mp.setValue(1,1,5)
+	var friendlies = mp.find_points(5)
+	mp.setValueAroundPoint(mp, friendlies[0][0], friendlies[0][1], 4)
+#5 Base
+#2 Supply
+#4 Allies
+#0 Default
+#Costly
+
+func supply_drop(x,y):
+	mp.setValue(x,y,2)
+
+func refresh_tiles():
+	for i in map_container.get_children():
+		map_container.remove_child(i)
+		i.queue_free()
 	
-	mp.setValue(starting_position_x,starting_position_y, 5)
+	for i in mp.array:
+		for j in i:
+			match j:
+				0:
+					create_texture_rect_for_map(default_texture, "default")
+				5:
+					create_texture_rect_for_map(base_texture, "base")
+				4:
+					create_texture_rect_for_map(friendly_texture, "friendly")
+				2:
+					create_texture_rect_for_map(supply_texture, "supply")
+				
+func create_texture_rect_for_map(texture:CompressedTexture2D, name: String):
+	var new_texture = TextureRect.new()
+	new_texture.name = name
+	new_texture.texture = texture
+	map_container.add_child(new_texture)
 	
-	mp.setValueAroundPoint(mp,starting_position_x,starting_position_y,true)
-	
-	#for i in mp.array:
-		#print(i)
+func scene_back_pressed():
+	var main_scene = get_node("/root/Main_Node/MainGame")
+	if main_scene != null:
+		main_scene.get_node("Main_Camera").make_current()
+		
+	if emit_signal("map_back") == ERR_UNAVAILABLE:
+		print("Failed to map_back signal")
 	
