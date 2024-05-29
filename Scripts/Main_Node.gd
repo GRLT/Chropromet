e275db25758a6d58317c895bb12fed744680b9c9
GRLT <laciok3@gmail.com>
2024-05-29 15:15:43 +0200

    Switching between the Morse scene and Main scene

--- Scripts/Main_Node.gd
+++ Scripts/Main_Node.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
 #var test_audio = preload("res://Assets/Music/output.mp3")
-@onready var audio_player = $AudioStreamPlayer2D
 
 @onready var warning_texture_on = load("res://Assets/Raid_Warning_0.png")
 @onready var warning_texture_off = load("res://Assets/Raid_Warning_1.png")
@@ -19,6 +18,10 @@ extends Node2D
 @onready var map_button = $MainGame/Stations/Map_Station/Map_Button
 @onready var map_camera = $Map/Map_Camera
 
+@onready var morse = $Morse
+@onready var morse_button = $MainGame/Stations/Morse_Station/Morse_Button
+@onready var morse_camera = $Morse/Morse_Camera
+
 @onready var raid_button = $MainGame/RaidButton
 @onready var warning_texture = $MainGame/Warning_Texture
 @onready var raid_camera = $Raid_s/RaidCamera
@@ -28,21 +31,27 @@ var switcher := Timer.new()
 
 func _ready():
 	chiper_button.pressed.connect(chiper_button_pressed)
-	
 	map_button.pressed.connect(map_button_pressed)
-	map.connect("map_back", map_back)
-	
 	raid_button.pressed.connect(raid_button_pressed)
+	morse_button.pressed.connect(morse_button_pressed)
+	
+	morse.morse_back.connect(morse_back)
+	map.map_back.connect(map_back)
 	raid_s.raid_back.connect(raid_button_back)
 
 	ProblemGenerator.connect("warning_raid", warning_raid)
 	chiper_view.connect("radio_info", radio_info)
 	chiper_view.connect("chiper_back", chiper_back)
 
+
+
 	add_child(switcher)
+func morse_back():
+	hide_unhide_with_expection(false, ["Raid_s", "Chiper", "Map", "Morse"])
 
 func map_back():
-	hide_unhide_with_expection(false, ["Raid_s", "ChiperRadioView", "Map"])
+	hide_unhide_with_expection(false, ["Raid_s", "Chiper", "Map"])
+
 
 func map_button_pressed():
 	hide_unhide_with_expection(true, ["Map"])
@@ -101,13 +110,18 @@ func radio_info(problem_queue_size: int, active:bool):
 	chiper_station_counter.text = str(problem_queue_size)
 	#radio_station_bool.texture = chiper_station_texture_bool 
 
+func morse_button_pressed():
+	hide_unhide_with_expection(true, ["Morse"])
+	morse_camera.make_current()
+	$Morse/SoundPlayer.set_volume_db(0)
+
 func raid_button_pressed():
 	hide_unhide_with_expection(true, ["Raid_s"])
 	raid_camera.make_current()
 	
 
 func raid_button_back():
-	hide_unhide_with_expection(false, ["Raid_s", "Chiper", "Map"])
+	hide_unhide_with_expection(false, ["Raid_s", "Chiper", "Map", "Morse"])
 
 func chiper_back():
 	hide_unhide_with_expection(false, ["Chiper"])
