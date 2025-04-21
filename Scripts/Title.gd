f94839c4aba2b9cc8774d1185bb8a0ba084dbc04
GRLT <laciok3@gmail.com>
2025-04-21 20:51:05 +0200

    Add Button for Title, for the new Scenes

--- Scripts/Title.gd
+++ Scripts/Title.gd
@@ -1,7 +1,9 @@
 extends Control
 
 @onready var start_button: Button = $CanvasLayer/VBoxContainer/Start_Button
+@onready var load_button: Button = $CanvasLayer/VBoxContainer/Load_Button
 @onready var options_button: Button = $CanvasLayer/VBoxContainer/Options_Button
+@onready var mission_editor: Button = $"CanvasLayer/VBoxContainer/Mission Editor"
 @onready var exit_button: Button = $CanvasLayer/VBoxContainer/Exit_Button
 
 
@@ -9,12 +11,20 @@ func _ready() -> void:
     start_button.pressed.connect(
         func() -> void:
         get_tree().change_scene_to_file("res://Scenes/Main_Scenes/Main_Node.tscn")
-        )
+    )
+    load_button.pressed.connect(
+        func() -> void:
+        get_tree().change_scene_to_file("res://Scenes/Main_Scenes/Load_Map.tscn")
+    )
     options_button.pressed.connect(
         func() -> void:
         get_tree().change_scene_to_file("res://Scenes/Main_Scenes/Options.tscn")
-        )
+    )
+    mission_editor.pressed.connect(
+        func() -> void:
+        get_tree().change_scene_to_file("res://Scenes/Main_Scenes/Mission_Creator.tscn")
+    )
     exit_button.pressed.connect(
         func() -> void:
         get_tree().quit(0)
-        )
+    )
