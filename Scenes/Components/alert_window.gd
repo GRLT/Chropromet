56a8173754a107561981f1f0451f488c494f23b5
GRLT <laciok3@gmail.com>
2025-04-26 00:36:11 +0200

    Correctly Save Settings into file
    Project file is change due to moving Prob_Gen and changed some texture
    stuff, so pixel art appears normally, without blur.

--- Scenes/Components/alert_window.gd
+++ Scenes/Components/alert_window.gd
@@ -1,11 +1,10 @@
 extends Node2D
-
+#This really should've been a class. Why did I did it like this?!
 
 signal save_to_file(input_data: LineEdit)
 
-
 @warning_ignore("unsafe_method_access", "unsafe_property_access")
-func init(state: String, error_code: int = 0) -> void:
+func init(state: String, code: int = 0) -> void:
     var window: Window = get_node("./Window")
     var scene: Node2D = get_node(".")
     
@@ -45,7 +44,7 @@ func init(state: String, error_code: int = 0) -> void:
         window.name = "Alert"
         button_event(button,scene, "Error")
         
-        match error_code:
+        match code:
             1:
                 label.text = "Error: You did not added any minigame!"
             2:
@@ -53,16 +52,32 @@ func init(state: String, error_code: int = 0) -> void:
             3:
                 label.text = "Error: You forgot to fill out a field!"
             _:
-                push_error("Error: Alert_Window failed to find an error code: ", error_code)
-                print("Error: Alert_Window failed to find an error code: ", error_code)
+                #Maybe Assert?
+                push_error("Error: Alert_Window failed to find an error code: ", code)
+                print("Error: Alert_Window failed to find an error code: ", code)
                 window.queue_free()
             #We need this because we can't just manually call a function
             #that'll redraw it properly and set the size AFTER the text was set
             ##https://github.com/godotengine/godot/issues/20623
-        label.set_size(label.get_minimum_size())
-            
+    elif state == "Info":
+        input_line.queue_free()
         
-    
+        window.name = "Info"
+        button_event(button,scene, "Info")
+        
+        match code:
+            1:
+                label.text = "Debug, you forgot to set a correct, code!"
+            2:
+                label.text = "You changed some values, are you sure you want to set them?"
+            3:
+                label.text = "Successfully Saved!"
+            _:
+                push_error("Error: Alert_Window failed to find an error code: ", code)
+                print("Error: Alert_Window failed to find an error code: ", code)
+                window.queue_free()
+        
+    label.set_size(label.get_minimum_size())
     overflow(window, label, 15.0)
     #For now 1152 and ~623 hard coded, due to how the game scales.
     @warning_ignore("integer_division")
@@ -89,6 +104,8 @@ func button_event(button: Button, scene: Node2D,state:String, input_line:LineEdi
     button.connect("pressed", 
     func() -> void:
         if state == "Error":
+            scene.queue_free()
+        elif state == "Info":
             scene.queue_free()
         elif state == "Save":
             if input_line.text == "":
--- Scripts/Options.gd
+++ Scripts/Options.gd
@@ -1,84 +1,103 @@
 extends Control
 
-@onready
-var window_mode_option: OptionButton = $CanvasLayer/BoxContainer/HWindow_Container/Window_Mode_Option
+@onready var scene: Node = get_node(".")
 
 @onready var back: Button = $CanvasLayer/BoxContainer/Back_Button
+@onready var save: Button = $CanvasLayer/BoxContainer/Save_Button
 
-@onready var volume_slider: HSlider = $CanvasLayer/BoxContainer/HVolume_Container/Volume_Slider
-@onready
-var volume_label_value: Label = $CanvasLayer/BoxContainer/HVolume_Container/Volume_Label_Value
+@onready var volume_label_value: Label = $CanvasLayer/BoxContainer/HVolume_Container/Volume_Label_Value
 
-@onready var max_fps_label: Label = $CanvasLayer/BoxContainer/HFPS_Container/Max_FPS_Label
+@onready var vsync: CheckButton = $CanvasLayer/BoxContainer/HVsync_Container/Vsync_Check
+@onready var window_mode: OptionButton = $CanvasLayer/BoxContainer/HWindow_Container/Window_Mode_Option
 @onready var max_fps_option: OptionButton = $CanvasLayer/BoxContainer/HFPS_Container/Max_FPS_Option
+@onready var volume_slider: HSlider = $CanvasLayer/BoxContainer/HVolume_Container/Volume_Slider
 
-@onready var vsync_check: CheckButton = $CanvasLayer/BoxContainer/HVsync_Container/Vsync_Check
 
-#Keybindings
-
+var state_changed:bool = false
 
 func _ready() -> void:
-	#Disables the option boxes
-	make_option_button_items_non_radio_checkable(window_mode_option)
-	make_option_button_items_non_radio_checkable(max_fps_option)
-
-	#Sets Max_FPS_Option's system default value
-	set_default_values()
-
-	#Connections
-	vsync_check.toggled.connect(vsync_check_toggled)
-	max_fps_option.item_selected.connect(max_fps_item_change)
-	window_mode_option.item_selected.connect(window_mode_item_change)
-	volume_slider.value_changed.connect(volume_slider_value_changed)
-	back.pressed.connect(back_button_pressed)
-
+    #Disables the option boxes
+    remove_radio_buttons(window_mode)
+    remove_radio_buttons(max_fps_option)
+    set_default_values()
+    
+    #Connections
+    vsync.toggled.connect(vsync_check_toggled)
+    max_fps_option.item_selected.connect(max_fps_item_change)
+    window_mode.item_selected.connect(window_mode_item_change)
+    volume_slider.value_changed.connect(volume_slider_value_changed)
+    back.pressed.connect(func() -> void: get_tree().change_scene_to_file("res://Scenes/Main_Scenes/Title.tscn"))
+    save.pressed.connect(save_pressed)
+    
+@warning_ignore("unsafe_method_access")
+func save_pressed() -> void:
+    var alert_win: = preload("res://Scenes/Components/Alert_Window.tscn").instantiate()
+    if state_changed:
+        alert_win.init("Info", 2)
+        scene.add_child(alert_win)
+        state_changed = false
+    else:
+        alert_win.init("Info", 3)
+        scene.add_child(alert_win)
+        Settings.save_settings(get_values())
+        #Reloades the config after changes so if scene changed, everything set correctly
+        Settings.load_settings()
 
 func set_default_values() -> void:
-	for i in max_fps_option.get_item_count():
-		if max_fps_option.get_item_text(i) == str(ceil(DisplayServer.screen_get_refresh_rate())):
-			max_fps_option.selected = i
-			continue
-	window_mode_option.selected = window_mode_option.get_item_index(DisplayServer.window_get_mode())
-	vsync_check.set_pressed(DisplayServer.window_get_vsync_mode())
-	Engine.set_max_fps(DisplayServer.screen_get_refresh_rate())
-	volume_label_value.text = str(int(volume_slider.value)) + "%"
+    vsync.button_pressed = Settings.vsync
+    window_mode.selected = window_mode.get_item_index(Settings.window_mode)
+    for i in max_fps_option.item_count:
+        if Settings.max_fps == int(max_fps_option.get_item_text(i)):
+            max_fps_option.selected = max_fps_option.get_item_id(i)
+            continue
+            
+    volume_slider.value = Settings.volume
+    volume_label_value.text = str(int(volume_slider.value)) + '%'
 
-
+func get_values() -> Array:
+    var values:Array = []
+    values.append(int(vsync.button_pressed))
+    values.append(window_mode.get_selected_id())
+    values.append(int(max_fps_option.get_item_text(max_fps_option.selected)))
+    values.append(volume_slider.value)
+    return values
+    
+    
 #https://www.reddit.com/r/godot/comments/oz45zd/anyway_to_remove_that_radio_button_check_on_the/
-func make_option_button_items_non_radio_checkable(option_button: OptionButton) -> void:
-	var pm: PopupMenu = option_button.get_popup()
-	for i in pm.get_item_count():
-		if pm.is_item_radio_checkable(i):
-			pm.set_item_as_radio_checkable(i, false)
-
+func remove_radio_buttons(option_button: OptionButton) -> void:
+    var pm: PopupMenu = option_button.get_popup()
+    for i in pm.get_item_count():
+        if pm.is_item_radio_checkable(i):
+            pm.set_item_as_radio_checkable(i, false)
 
 func vsync_check_toggled(state: bool) -> void:
-	if state:
-		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED)
-	else:
-		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+    if state:
+        DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED)
+    else:
+        DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
 
-
+    state_changed = true
+    
 func volume_slider_value_changed(value: int) -> void:
-	var current_volume: String = str(int(volume_slider.value))
+    var current_volume: String = str(int(volume_slider.value))
 
-	volume_label_value.text = current_volume + "%"
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), int(current_volume))
+    volume_label_value.text = current_volume + "%"
+    AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), int(current_volume))    
 
-
-func back_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Main_Scenes/Title.tscn")
-
-
+    state_changed = true
+    
 func max_fps_item_change(id: int) -> void:
-	Engine.set_max_fps(int(max_fps_option.get_item_text(id)))
-
-
-func window_mode_item_change(id: int) -> void:
-	match id:
-		0:
-			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-		1:
-			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
-		2:
-			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+    Engine.set_max_fps(int(max_fps_option.get_item_text(id)))
+    
+    state_changed = true
+    
+func window_mode_item_change(index: int) -> void:
+    var id:int = window_mode.get_item_id(index)
+    match id:
+        0:
+            DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+        3:
+            DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+        4:
+            DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+    state_changed = true
