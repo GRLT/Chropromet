985ae58be0556c816f1c894bca159b27e8d50f8c
GRLT <laciok3@gmail.com>
2025-04-30 16:29:35 +0200

    Raid Support multiple raid call from file

--- Scripts/Singleton/Raid.gd
+++ Scripts/Singleton/Raid.gd
@@ -7,6 +7,7 @@ extends Node
 var warning_timer:Timer = Timer.new()
 var raid_duration:Timer = Timer.new()
 var raid_wait: Timer = Timer.new()
+var check_timer: Timer = Timer.new()
 
 var audio_stream_player: AudioStreamPlayer = AudioStreamPlayer.new()
 var area_clear_player: AudioStreamPlayer = AudioStreamPlayer.new()
@@ -15,20 +16,27 @@ var area_clear_player: AudioStreamPlayer = AudioStreamPlayer.new()
 @onready var area_clear := preload("res://Assets/Sound/SFX/Area_Clear.mp3")
 
 func setup_timer() -> void:
+    check_timer.name = "check_timer"
     warning_timer.name = "warning_timer"
     raid_duration.name = "raid_duration"
     raid_wait.name = "raid_wait"
     
+    check_timer.one_shot = false
     warning_timer.one_shot = true
     raid_duration.one_shot = true
     raid_wait.one_shot = true
     
+    scene.add_child(check_timer)
+    check_timer.start(1)
     scene.add_child(warning_timer)
     scene.add_child(raid_duration)
     scene.add_child(raid_wait)
     
-var current_raid_object: Raid_Object
+func _process(delta: float) -> void:
+    print(raid_wait.time_left)
 
+var current_raid_object: Array[Raid_Object]
+var currently_selected_raid_object: Raid_Object
 func _ready() -> void:
     setup_timer()
     scene.add_child(audio_stream_player)
@@ -41,9 +49,9 @@ func _ready() -> void:
     
     raid_wait.timeout.connect(
         func() -> void:
-            warning_timer.start(current_raid_object.warning_timer)
+            warning_timer.start(currently_selected_raid_object.warning_timer)
             
-            raid_duration.start(current_raid_object.duration)
+            raid_duration.start(currently_selected_raid_object.duration)
             audio_stream_player.stream = siren
             audio_stream_player.play()
     )
@@ -69,8 +77,14 @@ func _ready() -> void:
             area_clear_player.stream = area_clear
             area_clear_player.play()
     )
-
+    
+    check_timer.timeout.connect(
+        func() -> void:
+            if warning_timer.time_left == 0 && raid_duration.time_left == 0 && raid_wait.time_left == 0 && current_raid_object.size() != 0:
+                currently_selected_raid_object = current_raid_object.pop_front()
+                raid_wait.start(currently_selected_raid_object.raid_start)
+    )
+    
 func raid_prep(current_raid_object: Raid_Object) -> void:
-    print("Here")
-    self.current_raid_object = current_raid_object
-    raid_wait.start(current_raid_object.raid_start)
+    self.current_raid_object.append(current_raid_object)
+    #raid_wait.start(current_raid_object.raid_start)
